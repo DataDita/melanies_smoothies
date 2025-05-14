659139b021ea2f467c6dd4ad8fe04c6066b2ab5c
DataDita <saba.safa@datadita.com>
2025-05-14 11:44:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,21 @@
+
+
+import streamlit as st
+import snowflake.snowpark as sp
+from snowflake.snowpark.functions import col
+cnx=st.connection("snowflake")
+session = cnx.session()
+from snowflake.snowpark import Session
+from datetime import datetime, timedelta
+import re
+import uuid
+import hashlib
+import base64
+from PIL import Image
+import io
+# Add this helper function with your imports
+from PIL import Image, ImageOps
+import io
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
